27e2fa320c2e6366aac311963a9aa759d907a633
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-07-07 14:24:57 +0900

    0707-03

--- step05/js/step05.js
+++ step05/js/step05.js
@@ -8,13 +8,22 @@ $(function () {
     });
 
     $('.mbtn').on('click', function () {
-        $('.gnb').toggleClass('on');
+        $('.gnb').toggleClass('on')
     });
 
     // $('.mbtn').on('click', () => ($('.gnb').toggleClass('on'), console.log(이솝)))
 
     $('.gnb>ul>li>a').on('click', function (e) {
-        e.preventDefault();
-        $(this).next().slideDown();
-    })
-})
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.smenu').stop().slideUp();
+            $(this).next().stop().slideToggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on');
+        $('.smenu').removeAttr('style');
+    });
+
+});
